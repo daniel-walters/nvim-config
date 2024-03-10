ae1b4fdfd852e01fbbb7cc3e0b434c64dcf5629f
danielwalters <waltersdaniel96@gmail.com>
2024-03-11 00:56:58 +1100

    Remove from function

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,35 +1,29 @@
-local M = {}
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
-M.init = function()
-  local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
 
-  if not vim.loop.fs_stat(lazypath) then
-    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-
-    vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  end
-
-  vim.opt.rtp:prepend(lazypath)
-
-  require('lazy').setup('core.plugins', {
-    ui = {
-      icons = vim.g.have_nerd_font and {} or {
-        cmd = '⌘',
-        config = '🛠',
-        event = '📅',
-        ft = '📂',
-        init = '⚙',
-        keys = '🗝',
-        plugin = '🔌',
-        runtime = '💻',
-        require = '🌙',
-        source = '📄',
-        start = '🚀',
-        task = '📌',
-        lazy = '💤 ',
-      },
-    },
-  })
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
 end
 
-return M
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup('core.plugins', {
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
