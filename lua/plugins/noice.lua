4355b846266727f8ee19fdbe3608b18eee9f01d1
danielwalters <waltersdaniel96@gmail.com>
2024-03-11 14:58:47 +1100

    Add noice

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,22 @@
+return {
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  opts = {
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+      },
+      presets = {
+        command_palette = true,
+        long_message_to_split = true,
+      },
+    },
+  },
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'rcarriga/nvim-notify',
+  },
+}
