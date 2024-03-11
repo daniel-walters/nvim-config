f3dfcfc92fc43008e81ed0717379819f90f9c044
danielwalters <waltersdaniel96@gmail.com>
2024-03-11 18:37:34 +1100

    update comment config

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,4 +1,13 @@
 return {
   'numToStr/Comment.nvim',
-  opts = {},
+  dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
+  keys = {
+    { 'gc', mode = { 'n', 'v' }, desc = 'Comment toggle linewise' },
+    { 'gb', mode = { 'n', 'v' }, desc = 'Comment toggle blockwise' },
+  },
+  opts = function()
+    return {
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    }
+  end,
 }
---
+++ lua/plugins/nvim-ts-context-commentstring.lua
@@ -0,0 +1,8 @@
+return {
+  'JoosepAlviste/nvim-ts-context-commentstring',
+  main = 'ts_context_commentstring',
+  lazy = true,
+  opts = {
+    enable_autocmd = false,
+  },
+}
