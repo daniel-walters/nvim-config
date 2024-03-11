41561ec2cb416d769942ed739947583481bdb5b2
danielwalters <waltersdaniel96@gmail.com>
2024-03-11 18:37:14 +1100

    update conform config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,19 +1,23 @@
-return { -- Autoformat
+-- INFO: https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters
+local prettier_filetypes =
+  { 'javacsript', 'javacsriptreact', 'typescript', 'typescriptreact' }
+
+local formatters_by_ft = { lua = { 'stylua' }, go = { 'gomft' } }
+
+for _, ft in pairs(prettier_filetypes) do
+  formatters_by_ft[ft] = { { 'prettierd', 'prettier' } }
+end
+
+return {
   'stevearc/conform.nvim',
+  event = 'BufWritepre',
+  cmd = 'ConformInfo',
   opts = {
-    notify_on_error = false,
+    notify_on_error = true,
     format_on_save = {
       timeout_ms = 500,
       lsp_fallback = true,
     },
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use a sub-list to tell conform to run *until* a formatter
-      -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
-    },
+    formatters_by_ft = formatters_by_ft,
   },
 }
