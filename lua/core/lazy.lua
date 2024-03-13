2a14e059b38112d5be6ebb29c240979e14d9fd79
danielwalters <waltersdaniel96@gmail.com>
2024-03-14 09:52:37 +1100

    Disable native plugins

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -3,12 +3,36 @@ local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
 
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--branch=stable',
+    lazyrepo,
+    lazypath,
+  }
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup('plugins', {
+  install = {
+    colorscheme = { 'tokyonight' },
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        'gzip',
+        'matchit',
+        'matchparen',
+        'netrwPlugin',
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
+      },
+    },
+  },
   ui = {
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
